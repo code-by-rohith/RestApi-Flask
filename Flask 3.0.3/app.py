54f1cbc4a9f4eba3ce647121f7cabbe18fdf1e8b
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-28 18:08:43 +0530

    add

--- Flask 3.0.3/app.py
+++ Flask 3.0.3/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  
 
 data1 = ['process', 'product', 'sales']
 
@@ -9,23 +10,29 @@ def main():
     data = ['process', 'product', 'sales']
     return render_template('index.html', data=data)
 
+
 @app.route('/<int:id>', methods=['GET'])
 def search(id):
     if id < len(data1):
         temp = data1[id]
+        flash('Successfully got the data!', 'success')  
         return render_template('search.html', temp=temp)
     else:
         return render_template('404.html')
 
+
 @app.route('/search', methods=['GET'])
 def search_by_query():
     query = request.args.get('query')
-    if query is not None and query.isdigit(): 
+    if query is not None and query.isdigit():
         id = int(query)
         if id < len(data1):
             temp = data1[id]
+            flash('Successfully got the data!', 'success') 
             return render_template('search.html', temp=temp)
+    flash('Invalid ID. Please try again.', 'danger')  
     return render_template('404.html')
+
 
 @app.template_filter('op')
 def upper(data):
@@ -36,7 +43,6 @@ def upper(data):
 def loop():
     loop = [i for i in range(100)]
     return render_template('loop.html', loop=loop)
-
 
 @app.errorhandler(404)
 def error(e):
