6a5bf1401bbe848a1589e13ee4ec8f4bb4b4cbed
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-10-05 19:42:23 +0530

    adD

--- JWT/JWT_KEY.py
+++ JWT/JWT_KEY.py
@@ -49,3 +49,4 @@ def protected():
 
 if __name__ == '__main__':
     app.run(debug=True)
+ 
---
+++ JWT/jwt-model.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, session, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token, decode_token, get_jwt_identity
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = "DADADA"
+app.config['SECRET_KEY'] = "s3wsw"
+jwt = JWTManager(app)
+
+@app.route('/')
+def main():
+    return render_template('index.html', access_token=None)
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    access_token = create_access_token(identity=username)
+    session['jwt'] = access_token
+    return render_template('index.html', access_token=access_token)
+
+@app.route('/protected', methods=['POST'])
+def protected():
+    token = request.form.get('token') 
+    if token: 
+        try:
+            decoded_token = decode_token(token)
+            username = decoded_token['sub']
+            time=decoded_token['iat']
+            return jsonify(message=f'Hello, {username}! and {time}'),201
+        except Exception:
+            return jsonify(message='Invalid token!'), 401
+    return jsonify(message='No token provided!'), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
